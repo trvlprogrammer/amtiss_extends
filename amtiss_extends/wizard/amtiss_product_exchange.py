d5e198f7a1526b285b1bb24b6a0d3d5b0514a35e
Alfatih Ridho NT <52704579+trvlprogrammer@users.noreply.github.com>
2022-11-15 16:38:33 +0700

    update wizard product exchange

--- amtiss_extends/wizard/amtiss_product_exchange.py
+++ amtiss_extends/wizard/amtiss_product_exchange.py
@@ -5,30 +5,11 @@ from odoo.exceptions import UserError
 class AmtissProductExchangeInherited(models.TransientModel):
     _inherit = "amtiss.product.exchange"
     
-    selected_product_ids = fields.Many2many(
-        "product.product","amtis_product_exchange_rel","product_exchange_id", "product_id",
+    selected_product_ids = fields.One2many(
+        "amtiss.product.exchange.detail","product_exchange_id",
         string="Selected Products"
     )
     
-    
-    @api.onchange('current_product_id')
-    def _onchange_current_product_ids(self):
-        for rec in self:
-            if rec.current_product_id:
-                current_product_variant_ids = self.env['product.product'].search([
-                    ('product_tmpl_id', '=', rec.current_product_id.product_tmpl_id.id),
-                    ('id', '!=', rec.current_product_id.id),
-                ], 
-                offset=0,                   #int
-                limit=None,                 #int
-                order=None,                 #string
-                )
-                return {
-                    'domain': {
-                        'selected_product_ids': [('id', 'in', current_product_variant_ids.ids)]
-                    }
-                }
-                
                 
     def action_replace_material_request_line(self):
         if len(self.selected_product_ids) > 1:
@@ -36,7 +17,91 @@ class AmtissProductExchangeInherited(models.TransientModel):
         elif len(self.selected_product_ids) == 0:
             raise UserError("You need to choose one product variant")
         
-        self.selected_product_id = self.selected_product_ids[0]
+        self.selected_product_id = self.selected_product_ids[0].name
         
         return super(AmtissProductExchangeInherited,self).action_replace_material_request_line()
+    
+    
+class AmtissProductExchangeDetail(models.TransientModel):
+    _name ="amtiss.product.exchange.detail"
+    
+    product_exchange_id = fields.Many2one("amtiss.product.exchange", string="Product Exchange")
+    name = fields.Many2one(string='Resource', comodel_name='product.product')
+    standard_price = fields.Float(string='Standard Price', related='name.standard_price')
+    uom_id = fields.Many2one(string='UoM', comodel_name='uom.uom', related='name.uom_id')
+    qty_available = fields.Float(string="QoH",comodel_name='qty_available')
+    quantities_at_locations_mapped = fields.Html(
+        string='Qty At Other Warehouses',
+        compute='_compute_quantities_at_locations_mapped',
+    )
+    stock_quant_ids = fields.Many2many(
+        string='Quantity-Warehouses',
+        comodel_name='stock.quant',
+        relation='material_request_line_stock_quant_rel',
+        column1='material_request_line_id',
+        column2='stock_quant_id',
+        compute="_compute_stock_quant_ids"
+    )
+    
+    
+    @api.onchange('product_exchange_id','product_exchange_id.current_product_id', 'name')
+    def _onchange_current_product_ids(self):
+        for rec in self:
+            if rec.product_exchange_id:
+                current_product_variant_ids = self.env['product.product'].search([
+                    ('product_tmpl_id', '=', rec.product_exchange_id.current_product_id.product_tmpl_id.id),
+                    ('id', '!=', rec.product_exchange_id.current_product_id.id),
+                ], 
+                offset=0,                   #int
+                limit=None,                 #int
+                order=None,                 #string
+                )
+                return {
+                    'domain': {
+                        'name': [('id', 'in', current_product_variant_ids.ids)]
+                    }
+                }
+                
+                
+    def get_quantity_by_location_id_and_product_id(self, products_id, location_id):
+        quantity = 0.0
+        stock_quant_ids = self.env['stock.quant'].search([
+            ('product_id', '=', products_id.id),
+            ('location_id', '=', location_id.id),
+        ], 
+        offset=0,                   #int
+        limit=1,                 #int
+        order=None,                 #string
+        )
+        if stock_quant_ids:
+            quantity = stock_quant_ids.inventory_quantity_auto_apply
+        return quantity
+                
+    @api.depends('picking_location_id')
+    def _compute_stock_on_picking_location_id(self):
+        for record in self:
+            record.stock_on_picking_location_id = record.get_quantity_by_location_id_and_product_id(
+                products_id= record.name,
+                location_id= record.picking_location_id
+            )
+    
+    @api.depends("name")
+    def _compute_stock_quant_ids(self):
+        for record in self:
+            self.stock_quant_ids = [(4, location_id.id) for location_id in self.env["stock.quant"].search([("product_id","=",self.name.id)])]
+    
+            
+    @api.depends('stock_quant_ids')
+    def _compute_quantities_at_locations_mapped(self):
+        for record in self:
+            quantities_at_locations_mapped = "<ul style='list-style-type: none;overflow: hidden;'>"
+            for stock_quant_id in record.stock_quant_ids:
+                if stock_quant_id.inventory_quantity_auto_apply:
+                    quantities_at_locations_mapped += \
+                        "<li style='border: 1px solid #eee;background-color: #eee;float: left;padding: 2px 5px;margin: 3px;border-radius: 10px;'>" + \
+                            str(stock_quant_id.inventory_quantity_auto_apply) + " @ " + \
+                            str(stock_quant_id.location_id.display_name) + \
+                        "</li>"
+            quantities_at_locations_mapped += '</ul>'
+            record.quantities_at_locations_mapped = quantities_at_locations_mapped
     
